af9173e533e61827abf8d569fa5e0439cb78e976
Daria Kozhemiakina <ihavebadfantasy@protonmail.com>
2021-03-23 18:32:30 +0200

    updated useUpdate to work with circular dependencies

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
-function usePrevious(value) {
-  const ref = useRef();
-
-  useEffect(() => {
-    ref.current = value;
-  });
-
-  return ref.current;
-}
-
-export function useUpdate(effect, dependencies = null) {
-  const [renderState, setRenderState] = useState('first render');
-  const [isPrevStateWasSet, setIsPrevStateWasSet] = useState(false);
-
-  const prevRenderState = usePrevious(renderState);
+export function useUpdate(effect, dependencies) {
+  const isFirstRender = useRef(true);
+  const prevDependencies = useRef(JSON.stringify(dependencies));
 
   useEffect(() => {
-    setRenderState('not first render');
-  }, []);
-
-  const dependenciesList = dependencies ? [...dependencies, renderState] : [renderState];
-
-  useEffect(() => {
-    if (!prevRenderState) {
-      return;
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+    } else {
+      if (prevDependencies.current !== JSON.stringify(dependencies)) {
+        prevDependencies.current = JSON.stringify(dependencies);
+        effect();
+      }
     }
-
-    if (prevRenderState && prevRenderState !== renderState) {
-      setIsPrevStateWasSet(true);
-
-      return;
-    }
-
-    effect();
-  }, dependenciesList);
-
-  useEffect(() => {
-  }, [prevRenderState]);
+  }, [dependencies]);
 }
