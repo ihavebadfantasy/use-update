c58ed9b52e36a315e221c7e45dca9762f1f65b36
Daria Kozhemiakina <ihavebadfantasy@protonmail.com>
2021-03-23 13:35:16 +0200

    added use-update-effect hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { useEffect, useState, useRef } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function usePrevious(value) {
+  const ref = useRef();
 
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
+
+export function useUpdateEffect(effect, dependencies = null) {
+  const [renderState, setRenderState] = useState('first render');
+  const [isPrevStateWasSet, setIsPrevStateWasSet] = useState(false);
+
+  const prevRenderState = usePrevious(renderState);
+
+  useEffect(() => {
+    setRenderState('not first render');
+  }, []);
+
+  const dependenciesList = dependencies ? [...dependencies, renderState, isPrevStateWasSet] : [renderState, isPrevStateWasSet];
+
+  useEffect(() => {
+    if (!prevRenderState) {
+      return;
+    }
+
+    if (prevRenderState && prevRenderState !== renderState) {
+      setIsPrevStateWasSet(true);
+
+      return;
+    }
+
+    effect();
+  }, dependenciesList);
+
+  useEffect(() => {
+  }, [prevRenderState]);
+}
